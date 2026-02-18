6a2f08459b568221ecf3880857163be0dd144a6c
Exequiel Ansaldi <exeansaldi0@gmail.com>
2026-02-17 22:50:44 -0300

    Implement DequeClient with basic operations

---
+++ TP3-EDD/ejercicio_cola/DequeClient.py
@@ -0,0 +1,23 @@
+from deque import Deque
+
+class DequeClient(Deque):
+    deque=Deque()
+    deque.add_first("Uno")
+    deque.add_first("Dos")
+    deque.add_first("Tres")
+    print("Deque con 3 elementos agregados desde el tope:\n",deque,"\n")
+    deque.add_last("Cuatro")
+    deque.add_last("Cinco")
+    deque.add_last("Seis")
+    print("Deque con 3 elementos agregados desde la cola:\n",deque,"\n")
+    print("Longitud:\n",len(deque),"\n")
+    print("Str:\n",deque.__str__(),"\n")
+    print("¿Está vacío?(is_empty):\n",deque.is_empty(),"\n")
+    print("Primer elemento:\n",deque.first(),"\n")
+    print("Ultimo elemento:\n",deque.last(),"\n")
+    print("Borrar el primer elemento:\n")
+    deque.delete_first()
+    print(deque)
+    print("Borrar el último elemento:\n")
+    deque.delete_last()
+    print(deque,"\n")
